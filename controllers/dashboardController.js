f3d327506cdc4ca66f57b5dd756181d06f25d10b
amogower <amogower@gmail.com>
2015-12-17 14:38:12 +0000

    Test

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -3,14 +3,14 @@ var mongoose = require('mongoose');
 
 function getActivities(req, res) {
   console.log(req.body.email);
-  User.findOne(req.body.email, function(err, user) {
+  User.findOne({local.email: req.body.email}, function(err, user) {
     if (err) {
       return res.status(404).json({
         message: 'Something went wrong showing the user' + err
       });
     }
     console.log(user);
-    console.log(user.local.activites);
+    console.log(user.local.activities);
     res.status(200).json({
       activities: user.local.activities
     });
